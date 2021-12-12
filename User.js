e0b1bebe7fc0b67fb294cd940e9efea25e942e90
Shahnawaz Tirmizi <tirmizishahnawaz@gmail.com>
2021-12-12 15:45:00 +0000

    Validating Schema

--- User.js
+++ User.js
@@ -1,9 +1,54 @@
 const mongoose = require('mongoose');
 
 // How to Create a Schema
+// const userSchema = new mongoose.Schema({
+//     name: String,
+//     age: Number,
+//     email: String,
+//     createdAt: Date,
+//     updatedAt: Date,
+//     bestFriend: mongoose.SchemaTypes.ObjectId,
+//     hobbies: [String],
+//     address: {
+//         street: String,
+//         city: String,
+//     }
+// })
+
+const addressSchema = new mongoose.Schema({
+    street: String,
+    city: String,
+})
+
 const userSchema = new mongoose.Schema({
     name: String,
-    age: Number
+    age: {
+        type: Number,
+        min: 1,
+        max: 100,
+        validate: {
+            validator: v => v % 2 === 0,
+            message: 'Age must be even'
+        }
+    },
+    email: {
+        type: String,
+        minlength: 5, 
+        required: true,
+        lowercase: true
+    },
+    createdAt: {
+        type: Date,
+        immutable: true,
+        default: () => Date.now()
+    },
+    updatedAt: {
+        type: Date,
+        default: () => Date.now()
+    },
+    bestFriend: mongoose.SchemaTypes.ObjectId,
+    hobbies: [String],
+    address: addressSchema
 })
 
 module.exports = mongoose.model('User', userSchema);
--- script.js
+++ script.js
@@ -8,26 +8,50 @@ mongoose.connect('mongodb://localhost/testdb', () => {
 }, e => console.log(e));
 
 
+// const run = async () => {
+//     // Two ways of creating users 
+//     // 1. Using the new keyword
+//     // const user = new User({
+//     // name: 'John',
+//     // age: 30
+//     // })
+//     // 2. Using the create method
+//     const user = await User.create({
+//         name: 'John',
+//         age: 30
+//     });
+
+
+//     // Upadte a user 
+//     user.name = 'Mike';
+//     await user.save();
+
+//     await user.save()
+//     console.log(user);
+// }
 const run = async () => {
-    // Two ways of creating users 
-    // 1. Using the new keyword
-    // const user = new User({
-    // name: 'John',
-    // age: 30
-    // })
-    // 2. Using the create method
-    const user = await User.create({
+    // creating a bit more complicated user
+    try {
+        const user = await User.create({
         name: 'John',
-        age: 30
+        age: 20,
+        email: "zi@test.COM",
+        hobbies: ['Sports', 'Cooking'],
+        address: {
+            street: 'Main St',
+            city: 'Boston',
+        }
     });
 
+        await user.save()
+        console.log(user);
+    } catch (error) {
+        console.log(error.message);
+    }
 
-    // Upadte a user 
-    user.name = 'Mike';
-    await user.save();
 
-    await user.save()
-    console.log(user);
 }
+
+
 
 run()
